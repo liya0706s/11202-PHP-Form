2621688090f111aefcb1944f78325e5374e0e295
Angie <liya0706s@gmail.com>
2023-11-22 23:04:56 +0800

    feat:add some notes on api/upload.php

--- api/upload.php
+++ api/upload.php
@@ -4,7 +4,7 @@
 
 
 echo $_POST['name'];
-// 輸出 POST 請求中的 'name' 參數的值，type=text
+// 輸出 POST 請求中的 'name' 參數的值，input type="text"
 
 echo "<br>";
 // xampp/tmp資料夾，暫時存放臨時文件的地方
@@ -15,24 +15,23 @@ if (!empty($_FILES['img']['tmp_name'])) {
     // 輸出上傳的文件的臨時文件名
     echo $_FILES['img']['tmp_name'];
     echo "<br>";
-    // 輸出上傳的文件的原始文件名
     echo $_FILES['img']['name'];
     echo "<br>";
-    // 輸出上傳的文件的類型
     echo $_FILES['img']['type'];
     echo "<br>";
-     // 輸出上傳的文件的大小
     echo $_FILES['img']['size'];
     // 更改檔名，不會跟別人撞名，容納規範
-    // 炸開黨名 年月日時間 隨機數字 .附檔名
     $tmp=explode(".",$_FILES['img']['name']);
-    // 以第一個參數做分割，img檔名
+    // 以第一個參數點點做分割，explode()陣列
+    // 炸開黨名 變成陣列中兩個元素 pic.jpg 
+
     $subname=".".end($tmp);
+    // end($tmp) 函數用於取得陣列 $tmp 的最後一個元素
     $filename=date("YmdHis").rand(10000,99999).$subname;
     // rand()..生成一個介於 10000 和 99999 之間的隨機整數
     move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $filename);
     // move_uploaded_file 將上傳的新文件搬移到，參數，從哪邊搬到哪邊
-    // 從tmp搬到 外面的img資料夾裡面
+    // move_uploaded_file(file, dest)
 
     header("location:../upload.php?img=".$filename);
     // api 傳值給前端upload顯示上傳的檔案
@@ -42,3 +41,13 @@ if (!empty($_FILES['img']['tmp_name'])) {
     }else{
     header("location:../upload.php?err=上傳失敗");
 }
+
+// 假設 $_FILES['img']['name'] 的值是 "example_file.jpg"，
+// 那麼 explode(".", $_FILES['img']['name']) 會產生以下的陣列：
+// Array
+// (
+//     [0] => example_file
+//     [1] => jpg
+// )
+// 陣列中的第一個元素是 "example_file"，第二個元素是 "jpg"。
+// 這樣你就能夠從這個陣列中取得檔案的副檔名，而在這個例子中，副檔名是 "jpg"。
