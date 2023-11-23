1a30a6d51a904196a98a0b9959bc74277b01a089
Angie <liya0706s@gmail.com>
2023-11-23 23:16:54 +0800

    feat:edit_file.php 判斷是否有id,檢查是否有新增檔案,有的話更新檔名/將檔案移動到指定目錄,根據檔案類型分類設變數,再判斷檔案類型是否有不一樣,不一樣的話更新檔名,更新文件類型和文件大小...

--- api/edit_file.php
+++ api/edit_file.php
@@ -8,31 +8,38 @@ include_once "../db.php";
 // 如果有上傳成功，函式內的變數例如tmp,subname,filename才存在
 // 沒上傳成功的話，檔名會是原檔名，不是$filename
 // $filename = date("YmdHis") . rand(10000, 99999) . $subname;
+
+// $_FILES['img']['name'] 原文件名稱;
+// 如果上傳的文件名 與 原文件名 不同
+// 更名後的新名稱 $file['name']
+// 用戶想要將文件命名的名稱 $_POST['name']  
+
+
+// 檢查是否設置了 'id' 的 POST 參數
 if(isset($_POST['id'])){
-    $file=find('files',$_POST['id'])
+    $file=find('files',$_POST['id']);
 }else{
-    
+    exit();
 }
 
-// 從資料庫拿檔案
-$file = find('files', $_GET['id']);
 
+// 檢查是否有上傳的檔案
 if (!empty($_FILES['img']['tmp_name'])) {
 
-    // 就檔名和新黨命
+    // 舊檔名和新檔名不同，則更新檔案名稱
     if($_POST['name']!=$file['name']){
-        move_uploaded_file();
-        file['']
+        file['name']=$_POST['name'];
         // 要重新指定$file，因為edit_file.php需要
     }
 
-    
+    // 將上傳的檔案移動到指定目錄
     move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $_POST['name']);
     // 以上資料夾下的檔名要改
     // filename to $_POST['name']
 
 
-
+    // 根據上傳檔案的類型進行分類判斷
+    // 建立變數名稱方便之後使用
     switch ($_FILES['img']['type']) {
         case "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
             $type = "msword";
@@ -50,26 +57,32 @@ if (!empty($_FILES['img']['tmp_name'])) {
         case "image/jpeg";
         case "image/png";
         case "image/gif";
-        case "";
-            $type = $_FILES[''][''];
+        case "image/bmp";
+            $type = $_FILES['img']['type'];
             break;
         default:
             $type = "other";
     }
     // 只處理檔案相關的type size file
-
-    if($type!=$file['type']){
-// 副檔名名稱不同的話，分為檔案和
-$file['type']=$type;
-
-// 檔名合成新富檔名
-
-
+    // 如果上傳的檔案類型與原文件不同，則更新文件類型和文件名稱。
+    if($type !=$file['type']){
+        $file['type'] = $type;
+         // 取得上傳檔案名稱的副檔名
+        $subname=end(explode(".", $_FILES['img']['name']));
+        // 將原文件名稱的副檔名替換為，上傳檔案的副檔名
+        $tmp = explode(".", $file['name']);
+        $tmp[count($tmp)-1]=$subname;
+         // 更新文件的名稱為替換後的名稱
+        $file['name']=join(".",$tmp);
     }
-
+// 更新文件類型和文件大小
+$file['type']=$type;
+$file['size']=$_FILES['img']['size'];
 
 }else{
-    if($_POST['name']=$file['name']){
+    // 如果沒有上傳檔案，但'name'與原文件不同，則更新文件名稱
+    if($_POST['name'] != $file['name']){
+        rename("../imgs/".$file['name'], "../imgs/".$_POST['name']);
 
 // 先改完名字，再賦值指定
 }
